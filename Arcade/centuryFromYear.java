49d06fe745b5839a36df85aef7b673af09042d10
Ruiyue Wang <phynew@hotmail.com>
2021-09-27 01:04:30 -0400

    Update centuryFromYear.java

--- Arcade/centuryFromYear.java
+++ Arcade/centuryFromYear.java
@@ -1,29 +1,29 @@
-Given a year, return the century it is in. 
-The first century spans from the year 1 up to and including the year 100, 
-the second - from the year 101 up to and including the year 200, etc.
+// Given a year, return the century it is in. 
+// The first century spans from the year 1 up to and including the year 100, 
+// the second - from the year 101 up to and including the year 200, etc.
 
-Example
+// Example
 
-For year = 1905, the output should be
-centuryFromYear(year) = 20;
-For year = 1700, the output should be
-centuryFromYear(year) = 17.
-Input/Output
+// For year = 1905, the output should be
+// centuryFromYear(year) = 20;
+// For year = 1700, the output should be
+// centuryFromYear(year) = 17.
+// Input/Output
 
-[execution time limit] 3 seconds (java)
+// [execution time limit] 3 seconds (java)
 
-[input] integer year
+// [input] integer year
 
-A positive integer, designating the year.
+// A positive integer, designating the year.
 
-Guaranteed constraints:
-1 ≤ year ≤ 2005.
+// Guaranteed constraints:
+// 1 ≤ year ≤ 2005.
 
-[output] integer
+// [output] integer
 
-The number of the century the year is in.
+// The number of the century the year is in.
   
-Solution 
+// Solution 
 int centuryFromYear(int year) {
     int ans = year / 100;
     int res = year % 100;
@@ -36,4 +36,4 @@ int centuryFromYear(int year) {
     }
 }
 
-TC: O(1); SC: O(1)
+// TC: O(1); SC: O(1)
