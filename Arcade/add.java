07893f93da007dfe9ab0c2b1a24a2671e6abf406
Ruiyue Wang <phynew@hotmail.com>
2021-09-27 01:02:50 -0400

    Update add.java

--- Arcade/add.java
+++ Arcade/add.java
@@ -1,31 +1,31 @@
-Write a function that returns the sum of two numbers.
+// Write a function that returns the sum of two numbers.
 
-Example
+// Example
 
-For param1 = 1 and param2 = 2, the output should be
-add(param1, param2) = 3.
+// For param1 = 1 and param2 = 2, the output should be
+// add(param1, param2) = 3.
 
-Input/Output
+// Input/Output
 
-[execution time limit] 3 seconds (java)
+// [execution time limit] 3 seconds (java)
 
-[input] integer param1
+// [input] integer param1
 
-Guaranteed constraints:
--1000 ≤ param1 ≤ 1000.
+// Guaranteed constraints:
+// -1000 ≤ param1 ≤ 1000.
 
-[input] integer param2
+// [input] integer param2
 
-Guaranteed constraints:
--1000 ≤ param2 ≤ 1000.
+// Guaranteed constraints:
+// -1000 ≤ param2 ≤ 1000.
 
-[output] integer
+// [output] integer
 
-The sum of the two inputs.
+// The sum of the two inputs.
   
-Solution
+// Solution
 int add(int param1, int param2) {
     return param1 + param2;
 }
 
-TC: O(1); SC: O(1)
+// TC: O(1); SC: O(1)
