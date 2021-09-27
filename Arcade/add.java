65e0d3527638fba738ad01ded0359e1a84f4a025
Ruiyue Wang <phynew@hotmail.com>
2021-09-27 01:02:27 -0400

    Create add.java

---
+++ Arcade/add.java
@@ -0,0 +1,31 @@
+Write a function that returns the sum of two numbers.
+
+Example
+
+For param1 = 1 and param2 = 2, the output should be
+add(param1, param2) = 3.
+
+Input/Output
+
+[execution time limit] 3 seconds (java)
+
+[input] integer param1
+
+Guaranteed constraints:
+-1000 ≤ param1 ≤ 1000.
+
+[input] integer param2
+
+Guaranteed constraints:
+-1000 ≤ param2 ≤ 1000.
+
+[output] integer
+
+The sum of the two inputs.
+  
+Solution
+int add(int param1, int param2) {
+    return param1 + param2;
+}
+
+TC: O(1); SC: O(1)
