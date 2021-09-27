a334ad091c0aa5fcdca09dc8d86e4236d9b9a431
Ruiyue Wang <phynew@hotmail.com>
2021-09-27 10:40:23 -0400

    Update shapeArea.java

--- Arcade/shapeArea.java
+++ Arcade/shapeArea.java
@@ -1,31 +1,31 @@
-Below we will define an n-interesting polygon. 
-Your task is to find the area of a polygon for a given n.
+// Below we will define an n-interesting polygon. 
+// Your task is to find the area of a polygon for a given n.
 
-A 1-interesting polygon is just a square with a side of length 1. 
-An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, 
-side by side. 
-You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+// A 1-interesting polygon is just a square with a side of length 1. 
+// An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, 
+// side by side. 
+// You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
 
-Example
+// Example
 
-For n = 2, the output should be
-shapeArea(n) = 5;
-For n = 3, the output should be
-shapeArea(n) = 13.
-Input/Output
+// For n = 2, the output should be
+// shapeArea(n) = 5;
+// For n = 3, the output should be
+// shapeArea(n) = 13.
+// Input/Output
 
-[execution time limit] 3 seconds (java)
+// [execution time limit] 3 seconds (java)
 
-[input] integer n
+// [input] integer n
 
-Guaranteed constraints:
-1 ≤ n < 104.
+// Guaranteed constraints:
+// 1 ≤ n < 104.
 
-[output] integer
+// [output] integer
 
-The area of the n-interesting polygon.
+// The area of the n-interesting polygon.
 
-Solution 
+// Solution 
 int shapeArea(int n) {
     if (n == 1) {
         return 1;
@@ -34,4 +34,4 @@ int shapeArea(int n) {
     return shapeArea(n - 1) + 4 * (n - 1); 
 }
 
-TC: O(n): SC: O(n)
+// TC: O(n): SC: O(n)
