a70ab6f85e644c53a9bc13d4f5ed21c9ead09a25
Ruiyue Wang <phynew@hotmail.com>
2021-09-28 00:11:24 -0400

    Update Make Array Consecutive 2.java

--- Arcade/Make Array Consecutive 2.java
+++ Arcade/Make Array Consecutive 2.java
@@ -1,34 +1,34 @@
-Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, 
-each statue having an non-negative integer size. Since he likes to make things perfect, 
-he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. 
-He may need some additional statues to be able to accomplish that. 
-Help him figure out the minimum number of additional statues needed.
+// Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, 
+// each statue having an non-negative integer size. Since he likes to make things perfect, 
+// he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. 
+// He may need some additional statues to be able to accomplish that. 
+// Help him figure out the minimum number of additional statues needed.
 
-Example
+// Example
 
-For statues = [6, 2, 3, 8], the output should be
-makeArrayConsecutive2(statues) = 3.
+// For statues = [6, 2, 3, 8], the output should be
+// makeArrayConsecutive2(statues) = 3.
 
-Ratiorg needs statues of sizes 4, 5 and 7.
+// Ratiorg needs statues of sizes 4, 5 and 7.
 
-Input/Output
+// Input/Output
 
-[execution time limit] 3 seconds (java)
+// [execution time limit] 3 seconds (java)
 
-[input] array.integer statues
+// [input] array.integer statues
 
-An array of distinct non-negative integers.
+// An array of distinct non-negative integers.
 
-Guaranteed constraints:
-1 ≤ statues.length ≤ 10,
-0 ≤ statues[i] ≤ 20.
+// Guaranteed constraints:
+// 1 ≤ statues.length ≤ 10,
+// 0 ≤ statues[i] ≤ 20.
 
-[output] integer
+// [output] integer
 
-The minimal number of statues that need to be added to existing statues 
-such that it contains every integer size from an interval [L, R] (for some L, R) and no other sizes.
+// The minimal number of statues that need to be added to existing statues 
+// such that it contains every integer size from an interval [L, R] (for some L, R) and no other sizes.
 
-Solution
+// Solution
 int makeArrayConsecutive2(int[] statues) {
     if (statues.length == 1) {
         return 0;
@@ -45,4 +45,4 @@ int makeArrayConsecutive2(int[] statues) {
     return max - min + 1 - statues.length;
 }
 
-TC: O(n); SC: O(1)
+// TC: O(n); SC: O(1)
